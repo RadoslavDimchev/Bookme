0fe4a88b70534f151f5fcf6ed5c0e57741a63d62
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2022-12-31 12:17:28 +0200

    Implemented Catalog, Details and not found rooom

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -1,16 +1,31 @@
+const { getAll, getById } = require('../services/accommodationServise');
+
 const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
+  const rooms = getAll();
   res.render('catalog', {
-    title: 'All Accommodation'
+    title: 'All Accommodation',
+    rooms
   });
 });
 
 router.get('/:id', (req, res) => {
-  res.render('details', {
-    title: 'Accommodation Details'
-  });
+  const id = req.params.id;
+  const room = getById(id);
+
+  if (room) {
+    res.render('details', {
+      title: 'Accommodation Details',
+      room
+    });
+  } else {
+    res.render('roomNotFound', {
+      title: 'Accommodation Details',
+      id
+    });
+  }
 });
 
 module.exports = router;
