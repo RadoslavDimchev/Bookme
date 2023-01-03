8b2330217452c51cfa06e424549a4fa0e3f1d533
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-01-04 01:20:16 +0200

    Created userNav middleware

--- config/express.js
+++ config/express.js
@@ -3,6 +3,7 @@ const hbs = require('express-handlebars').create({ extname: '.hbs' });
 const cookieParser = require('cookie-parser');
 const defaultTitle = require('../middlewares/defaultTitle');
 const auth = require('../middlewares/auth');
+const userNav = require('../middlewares/userNav');
 
 
 const jwtSecret = 'vbcmhgfmf';
@@ -15,6 +16,7 @@ module.exports = (app) => {
   app.use('/static', express.static('static'));
   app.use(cookieParser());
   app.use(auth(jwtSecret));
+  app.use(userNav());
 
   app.use(defaultTitle('Bookme Accommodation'));
 };
