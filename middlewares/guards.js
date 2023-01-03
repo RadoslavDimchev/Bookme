d34e0c8d98b4a083ffff15ebd17e369a723bf5b9
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-01-04 01:33:25 +0200

    Created guards middlewares

---
+++ middlewares/guards.js
@@ -0,0 +1,38 @@
+function hasUser() {
+  return (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect('/auth/login');
+    }
+  };
+}
+
+function isGuest() {
+  return (req, res, next) => {
+    if (!req.user) {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  };
+}
+
+function hasRole(role) {
+  return (req, res, next) => {
+    if (req.user === undefined) {
+      return res.redirect('/auth/login');
+    }
+    if (req.user.roles.includes(role) === false) {
+      return res.redirect('/auth/login');
+    }
+
+    next();
+  };
+}
+
+module.exports = {
+  hasUser,
+  isGuest,
+  hasRole
+};
