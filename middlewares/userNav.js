265f76304bfc360db4192aa6a1b64e89b71fcac4
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-01-15 22:37:48 +0200

    Finish the style

--- middlewares/userNav.js
+++ middlewares/userNav.js
@@ -2,6 +2,9 @@ module.exports = () => (req, res, next) => {
   if (req.user !== undefined) {
     res.locals.hasUser = true;
     res.locals.username = req.user.username;
+    if (req.user.roles.includes('admin')) {
+      res.locals.isAdmin = true;
+    }
   }
   next();
 };
